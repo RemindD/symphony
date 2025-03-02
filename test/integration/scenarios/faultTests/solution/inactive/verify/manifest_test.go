fdbb19f88e9a0beeafdc60aa65fe6abd8234b4f6
Xingdong Li <xingdli@microsoft.com>
2025-03-02 22:27:52 +0800

    fixup

--- test/integration/scenarios/faultTests/solution/inactive/verify/manifest_test.go
+++ test/integration/scenarios/faultTests/solution/inactive/verify/manifest_test.go
@@ -10,6 +10,9 @@ import (
 	"testing"
 )
 
+// 1. Deploy instance as inactive. Verify that the instance is inactive and deployment is not created.
+// 2. Change instance as active. inject failpoint to postpone the deployment.
+// 3. Change instance as inactive. Verify that the deployment is cleaned up.
 func TestInstance_InActive(t *testing.T) {
 
 }
