b973ba88735f5125f864eeada1c9b5a51e870e5c
Xingdong <xdlisjtu@gmail.com>
2025-03-02 10:20:04 +0800

    fault tests

---
+++ test/integration/scenarios/faultTests/solution/inactive/verify/manifest_test.go
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) Microsoft Corporation.
+ * Licensed under the MIT license.
+ * SPDX-License-Identifier: MIT
+ */
+
+package verify
+
+import (
+	"testing"
+)
+
+func TestInstance_InActive(t *testing.T) {
+
+}
