be2f485de01b8b658ced283c50a360776fa594d5
Xingdong Li <xingdli@microsoft.com>
2025-03-07 10:34:27 +0800

    [workaround] skip instanceHistory validateUpdate to unblock hydration

--- k8s/apis/solution/v1/instancehistory_webhook.go
+++ k8s/apis/solution/v1/instancehistory_webhook.go
@@ -111,10 +111,8 @@ func (r *InstanceHistory) ValidateUpdate(old runtime.Object) (admission.Warnings
 	operationName := fmt.Sprintf("%s/%s", constants.InstanceHistoryOperationNamePrefix, constants.ActivityOperation_Write)
 	ctx := configutils.PopulateActivityAndDiagnosticsContextFromAnnotations(r.GetNamespace(), resourceK8SId, r.Annotations, operationName, historyReaderClient, context.TODO(), historyLog)
 
-	// instance history is readonly and should not be updated
-	err := fmt.Errorf("Cannot update instance history because it is readonly")
-	diagnostic.ErrorWithCtx(historyLog, ctx, err, "Instance history is readonly", "name", r.Name, "namespace", r.Namespace)
-	return nil, err
+	diagnostic.InfoWithCtx(historyLog, ctx, "Instance history is readonly", "name", r.Name, "namespace", r.Namespace)
+	return nil, nil
 }
 
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
